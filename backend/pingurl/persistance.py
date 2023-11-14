d9daeb36f924479047aef604af253f08e8a30f99
JustOwl <ka03bj@gmail.com>
2023-11-14 10:39:18 +0100

    Major change

--- backend/pingurl/persistance.py
+++ backend/pingurl/persistance.py
@@ -2,90 +2,44 @@ from pingurl.models import WatchedUrl, PingData
 
 watched_urls = {}
 pings = {}
-NEXT_ID = 0
-
+next_id = 0
 
 def add_watched_url(watched_url: WatchedUrl):
     if not isinstance(watched_url, WatchedUrl):
         raise ValueError("watched_url must be a WatchedUrl instance")
 
-    if watched_url.url_id is not None:
+    if (watched_url.url_id is not None):
         raise ValueError("url_id must be None")
-    global NEXT_ID
+    global next_id
 
-    watched_url.url_id = NEXT_ID
+    watched_url.url_id = next_id
 
-    NEXT_ID += 1
+    next_id += 1
 
     watched_urls[watched_url.url_id] = watched_url
 
     return watched_url.url_id
 
-
-def get_watched_url(url_id):
+def delete_url(url_id):
     if not isinstance(url_id, int):
         raise ValueError("url_id must be an integer")
-
-    if not url_id in watched_urls:
-        raise WatchedUrlNotFoundError("url_id not found")
-
-    return watched_urls[url_id]
-
-
-def delete_watched_url(url_id):
-    if not isinstance(url_id, int):
-        raise ValueError("url_id must be an integer")
-
-    if not url_id in watched_urls:
-        raise WatchedUrlNotFoundError("url_id not found")
+    
+    if url_id not in watched_urls:
+        raise ItemNotFoundError("url_id not found")
 
     del watched_urls[url_id]
 
-
 def get_url_data(url_id):
-    if not isinstance(url_id, int):
-        raise ValueError("url_id must be an integer")
-
-    if not url_id in watched_urls:
-        raise WatchedUrlNotFoundError("url_id not found")
-
-    watched_url_dict = watched_urls[url_id].to_dict()
-
-    url_pings = pings.get(url_id, [])
-
-    # turn the list of url pings into a list of dicts without the urlId key
-    url_pings_result = [
-        {k: v for k, v in ping.to_dict().items() if k != "urlId"} for ping in url_pings
-    ]
-
-    return {**watched_url_dict, "pings": url_pings_result}
-
+    return jsonify({'message': 'Good'}), 201
 
 def get_url_ids():
     return list(watched_urls.keys())
 
-
-def add_ping_data(ping_data: PingData):
-    if not isinstance(ping_data, PingData):
-        raise ValueError("ping_data must be a PingData instance")
-
-    if not ping_data.url_id in watched_urls:
-        raise WatchedUrlNotFoundError("Watched URL with the given urlId was not found.")
-
-    if not ping_data.url_id in pings:
-        pings[ping_data.url_id] = []
-
-    pings[ping_data.url_id].append(ping_data)
-
-
 def get_stats():
-    ping_count = 0
-    for url_id in pings:
-        ping_count += len(pings[url_id])
+    return {'watchedUrls': len(watched_urls), 'pings': len(pings)}
 
-    return {"watchedUrls": len(watched_urls), "pings": ping_count}
-
-
-class WatchedUrlNotFoundError(Exception):
-    def __init__(self, message):
-        super().__init__(message)
+class ItemNotFoundError(Exception):
+    def __init__(self, id, message="Watched URL with the given urlId was not found."):
+        self.id = id
+        self.message = message
+        super().__init__(self.message)
---
+++ backend/pingurl/routes/__init__.py
@@ -0,0 +1,4 @@
+from .watched_urls import add_watched_url
+from .watched_urls import delete_url
+from .watched_urls import get_url_data
+from .watched_urls import get_url_ids
