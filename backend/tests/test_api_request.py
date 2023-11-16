8fb7005454ef322200015b1cca791526de63e74b
JustOwl <ka03bj@gmail.com>
2023-11-16 14:15:54 +0100

    awdawd

--- backend/tests/test_api_request.py
+++ backend/tests/test_api_request.py
@@ -3,8 +3,8 @@ import requests
 
 container_ip = os.environ.get('CONTAINER_IP', 'default_value_if_not_set')
 
-API_WATCHED_URL = f"http://{container_ip}:5000/watched-urls"
-API_STATS_URL = f"http://{container_ip}:5000/stats"
+API_WATCHED_URL = f"http://localhost:5000/watched-urls"
+API_STATS_URL = f"http://localhost:5000/stats"
 
 def test_get_watched_url_none():
     assert requests.get(API_WATCHED_URL, timeout=20).json() == {'urlIds': []}
