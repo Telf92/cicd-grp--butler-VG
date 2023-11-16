8b8744247fa27d862bc046d50d734bced9fd1f62
JustOwl <ka03bj@gmail.com>
2023-11-16 10:04:10 +0100

    changed IP

--- jenkins/tests/test_api_request.py
+++ jenkins/tests/test_api_request.py
@@ -1,7 +1,7 @@
 import requests
 
-API_WATCHED_URL = "http://127.0.0.1:5000/watched-urls"
-API_STATS_URL = "http://127.0.0.1:5000/stats"
+API_WATCHED_URL = "http://0.0.0.0:5000/watched-urls"
+API_STATS_URL = "http://0.0.0.0:5000/stats"
 
 def test_get_watched_url_none():
     assert requests.get(API_WATCHED_URL, timeout=20).json() == {'urlIds': []}
