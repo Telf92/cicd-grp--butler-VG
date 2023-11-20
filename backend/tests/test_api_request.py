5de9c6c86c840c18995a0d0ce54f063fa113b6a1
JustOwl <ka03bj@gmail.com>
2023-11-20 13:26:55 +0100

    revert to localhost

--- backend/tests/test_api_request.py
+++ backend/tests/test_api_request.py
@@ -1,10 +1,7 @@
 import os
 import requests
 
-container_ip = os.environ.get('CONTAINER_IP', 'default_value_if_not_set')
-
-API_URL = os.environ.get('API_URL', 'default_value_if_not_set')
-#API_URL = f"http://{container_ip}:5001"
+API_URL = f"http://localhost:5001"
 
 # Just made one universal constant that then gets added onto when needed
 #API_WATCHED_URL = "http://localhost:5000/watched-urls"
