ea8f5f10fad63015dc54a8caeec1708c5b004ba0
JustOwl <ka03bj@gmail.com>
2023-11-20 13:37:01 +0100

    fixed Jarl's dumb formatting

--- backend/tests/test_api_request.py
+++ backend/tests/test_api_request.py
@@ -1,13 +1,15 @@
 import requests
 
-API_WATCHED_URL = "http://localhost:5000/watched-urls"
-API_STATS_URL = "http://localhost:5000/stats"
 
-API_DELETE_ULR = "http://localhost:5000/watched-urls/0"
+API_URL = "http://localhost:5000"
+
+# API_WATCHED_URL = "http://localhost:5000/watched-urls"
+# API_STATS_URL = "http://localhost:5000/stats"
+# API_DELETE_ULR = "http://localhost:5000/watched-urls/0"
 
 
 def test_get_watched_url_none():
-    assert requests.get(API_WATCHED_URL, timeout=20).json() == {"urlIds": []}
+    assert requests.get(f"{API_URL}/watched-urls", timeout=20).json() == {"urlIds": []}
 
 
 def test_set_watched_url():
@@ -17,23 +19,23 @@ def test_set_watched_url():
         "periodSec": 30,
         "url": "https://google.com",
     }
-    assert requests.post(API_WATCHED_URL, json=to_post, timeout=20).json() == {
+    assert requests.post(f"{API_URL}/watched-urls", json=to_post, timeout=20).json() == {
         "message": "Watched URL added",
         "urlId": 0,
     }
 
 
 def test_get_watched_url_first():
-    assert requests.get(API_WATCHED_URL, timeout=20).json() == {"urlIds": [0]}
+    assert requests.get(f"{API_URL}/watched-urls", timeout=20).json() == {"urlIds": [0]}
 
 
 def test_get_stats():
-    assert requests.get(API_STATS_URL, timeout=20).json() == {
+    assert requests.get(f"{API_URL}/stats", timeout=20).json() == {
         "pings": 1,
         "watchedUrls": 1,
     }
 
 def test_delete_url():
-    assert requests.delete(API_DELETE_ULR, timeout=20).json() == {
+    assert requests.delete(f"{API_URL}/watched-urls/0", timeout=20).json() == {
         "message": "Removed watched url with id 0"
     }
