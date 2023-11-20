39340df1f2e18dc196eaa7878496f4d59e54ee13
JustOwl <ka03bj@gmail.com>
2023-11-20 12:45:00 +0100

    Update to jenkinsfile and test

--- backend/tests/test_api_request.py
+++ backend/tests/test_api_request.py
@@ -2,15 +2,17 @@ import os
 import requests
 
 container_ip = os.environ.get('CONTAINER_IP', 'default_value_if_not_set')
-API_WATCHED_URL = f"http://{container_ip}:5000/watched-urls"
-#API_WATCHED_URL = "http://localhost:5000/watched-urls"
-API_STATS_URL = "http://localhost:5000/stats"
 
-API_DELETE_ULR = "http://localhost:5000/watched-urls/0"
+API_URL = f"http://{container_ip}:5001/"
+
+# Just made one universal constant that then gets added onto when needed
+#API_WATCHED_URL = "http://localhost:5000/watched-urls"
+#API_STATS_URL = f"http:{container_ip}:5001/stats"
+#API_DELETE_ULR = f"http://{container_ip}:5000/watched-urls/0"
 
 
 def test_get_watched_url_none():
-    assert requests.get(API_WATCHED_URL, timeout=20).json() == {"urlIds": []}
+    assert requests.get(f"{API_URL}watched-urls", timeout=20).json() == {"urlIds": []}
 
 
 def test_set_watched_url():
@@ -20,23 +22,23 @@ def test_set_watched_url():
         "periodSec": 30,
         "url": "https://google.com",
     }
-    assert requests.post(API_WATCHED_URL, json=to_post, timeout=20).json() == {
+    assert requests.post(f"{API_URL}/watched-urls", json=to_post, timeout=20).json() == {
         "message": "Watched URL added",
         "urlId": 0,
     }
 
 
 def test_get_watched_url_first():
-    assert requests.get(API_WATCHED_URL, timeout=20).json() == {"urlIds": [0]}
+    assert requests.get(f"{API_URL}/watched-urls", timeout=20).json() == {"urlIds": [0]}
 
 
 def test_get_stats():
-    assert requests.get(API_STATS_URL, timeout=20).json() == {
+    assert requests.get(f"{API_URL}/stats", timeout=20).json() == {
         "pings": 1,
         "watchedUrls": 1,
     }
 
 def test_delete_url():
-    assert requests.delete(API_DELETE_ULR, timeout=20).json() == {
+    assert requests.delete(f"{API_URL}/watched-urls/0", timeout=20).json() == {
         "message": "Removed watched url with id 0"
     }
