4c501d7f744e9fef07d45eb8251ceb4e8df10187
JustOwl <ka03bj@gmail.com>
2023-11-20 09:52:03 +0100

    Added the script with fixes since the old bricked

---
+++ local-pipeline.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+
+echo "=============
+  Create and activate Venv  
+=============" 
+
+python3 -m venv backend/venv
+source backend/venv/bin/activate
+pip install -r backend/requirements.txt
+
+echo "=============
+  Run Black  
+=============" 
+
+echo "Skipping Black, will brick project"
+#black backend/
+
+
+echo "=============
+  Run PyLint  
+============="
+
+python3 -m pylint --fail-under=7 --recursive true --ignore=venv backend
+
+echo "=============
+  Build Docker Image  
+============="
+
+docker build -t ping-url .
+
+echo "=============
+  Run Docker Image  
+============="
+
+CONTAINER_ID=$(docker run --network=host -d ping-url)
+sleep 3
+
+echo "=============
+  Run Tests  
+============="
+
+pytest backend/tests/test_api_request.py
+
+echo "=============
+  Stop and Remove Containers  
+============="
+
+docker stop $CONTAINER_ID
+docker rm $CONTAINER_ID
