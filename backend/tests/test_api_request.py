3bee6900d65eaa745f73f816fd9a54752dd78226
Jarl Jakobsson <jarl.jakobsson@gmail.com>
2023-11-20 11:14:21 +0100

    jarl-api-test added

--- backend/tests/test_api_request.py
+++ backend/tests/test_api_request.py
@@ -3,6 +3,8 @@ import requests
 API_WATCHED_URL = "http://localhost:5000/watched-urls"
 API_STATS_URL = "http://localhost:5000/stats"
 
+API_DELETE_ULR = "http://localhost:5000/watched-urls/0"
+
 
 def test_get_watched_url_none():
     assert requests.get(API_WATCHED_URL, timeout=20).json() == {"urlIds": []}
@@ -29,4 +31,9 @@ def test_get_stats():
     assert requests.get(API_STATS_URL, timeout=20).json() == {
         "pings": 1,
         "watchedUrls": 1,
+    }
+
+def test_delete_url():
+    assert requests.delete(API_DELETE_ULR, timeout=20).json() == {
+        "message": "Removed watched url with id 0"
     }
